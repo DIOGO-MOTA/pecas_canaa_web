2b6b311ca7933ec43fb2323dd6d692d6354e89d1
DIOGO-MOTA <diogomota21@gmail.com>
2022-09-19 15:11:49 -0300

    19/09 15:11

--- src/components/SectionsCardBrands/index.tsx
+++ src/components/SectionsCardBrands/index.tsx
@@ -6,13 +6,13 @@ import "keen-slider/keen-slider.min.css";
 import { Box, Flex } from "@chakra-ui/react";
 import Image from "next/future/image";
 
-const animation = { duration: 30000, easing: (t: number) => t };
+const animation = { duration: 9000, easing: (t: number) => t };
 
 export function SectionsCardBrands() {
   const [sliderRef] = useKeenSlider<HTMLDivElement>({
     slides: {
       perView: 7,
-      spacing: 80,
+      spacing: 30,
     },
     loop: true,
     renderMode: "performance",
@@ -32,21 +32,27 @@ export function SectionsCardBrands() {
       <Box  className="keen-slider__slide">
         <Image src="/chevrolet.png" width={177} height={100} alt="" />
       </Box>
+
       <Box  className="keen-slider__slide">
         <Image src="/fiat.png" width={177} height={100} alt="" />
       </Box>
+
       <Box  className="keen-slider__slide">
         <Image src="/ford.png" width={177} height={100} alt="" />
       </Box>
+
       <Box  className="keen-slider__slide">
         <Image src="/honda.png" width={177} height={100} alt="" />
       </Box>
+
       <Box  className="keen-slider__slide">
         <Image src="/hyundai.png" width={177} height={100} alt="" />
       </Box>
+
       <Box  className="keen-slider__slide">
         <Image src="/toyota.png" width={177} height={100} alt="" />
       </Box>
+      
       <Box  className="keen-slider__slide">
         <Image src="/volkswagen.png" width={177} height={100} alt="" />
       </Box>
